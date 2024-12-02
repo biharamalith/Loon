bd1437ac3bcd260f0c9b101ffe7b2fe86096b507
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-12-03 01:31:42 +0530

    Added Necessary Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +60,31 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation (libs.androidx.material.v151)
+    implementation (libs.ui)
+    implementation (libs.coil.compose.v230)
+
+    // Compose Dependencies
+    implementation (libs.ui)
+    implementation (libs.androidx.compose.material.material)
+    implementation (libs.ui.tooling.preview)
+    implementation (libs.androidx.activity.compose.v181)
+
+    // Update or add these compose-related dependencies
+    implementation(platform(libs.androidx.compose.bom.v20231001))
+    implementation(libs.androidx.compose.ui.ui2)
+    implementation(libs.ui.graphics)
+    implementation(libs.material3)
+    implementation(libs.androidx.compose.ui.ui.tooling.preview)
+    implementation(libs.androidx.runtime)
+
+    // If you need image loading capabilities
+    implementation(libs.coil.kt.coil.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +92,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+
+
 }
