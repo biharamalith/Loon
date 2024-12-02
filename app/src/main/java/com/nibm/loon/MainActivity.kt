9d8330c41fa5e5628b8f7cac548d531fbee61430
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-12-03 02:24:15 +0530

    Initial MainActivity.kt

--- app/src/main/java/com/nibm/loon/MainActivity.kt
+++ app/src/main/java/com/nibm/loon/MainActivity.kt
@@ -3,45 +3,13 @@ package com.nibm.loon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.nibm.loon.ui.theme.LoonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            LoonTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text("Welcome to the Salon Appointment Booking App!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoonTheme {
-        Greeting("Android")
     }
 }
