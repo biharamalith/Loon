c6247d4d36e19bf477c173950ffbaeaa8fc4718d
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-12-29 01:27:49 +0530

    Added Color Variables for UI Color Palette

---
+++ app/src/main/java/com/nibm/loon/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.nibm.loon.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenColor = Color(0xFF75E6A4)
+
+val LightGreenColor = Color(0xFF5AE3A8)
+val DarkGreenColor = Color(0xFF273838)
+val LightGrayGreenColor = Color(0xFFB0C5BB)
+val BlackColor = Color(0xFF000000)
